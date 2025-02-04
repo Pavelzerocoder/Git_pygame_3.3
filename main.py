da6ed8f582a276921bca77c1506f187b02aec64a
Nika6s <nika.samofrakian@gmail.com>
2025-02-04 21:41:51 +0500

    add all const

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 pygame.init()
@@ -7,7 +9,20 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Игра тир')
-icon = pygame.image.load('https://avatars.mds.yandex.net/i?id=dc81fdde28d6825e003e441af024dacc60b59e14-4255743-images-thumbs&n=13')
+icon = pygame.image.load('img/pic_icon.jpg')
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load('img/target.png')
+target_wight = 50
+target_height = 50
+
+target_x = random.randint(0, SCREEN_WIDTH - target_wight)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+
 
 running = True
 while running:
@@ -15,4 +30,3 @@ while running:
 
 pygame.quit()
 
-'https://avatars.mds.yandex.net/i?id=dc81fdde28d6825e003e441af024dacc60b59e14-4255743-images-thumbs&n=13'
